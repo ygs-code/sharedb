9c2269fe2394d39d05c9dda11255fe78267eb3ea
姚观寿 <yaoguanshou@rainbowcn.com>
2022-04-02 19:29:38 +0800

    修改编译配置

--- examples/textarea/client.js
+++ examples/textarea/client.js
@@ -1,36 +1,45 @@
-var sharedb = require('sharedb/lib/client');
-var StringBinding = require('sharedb-string-binding');
-
+/*
+ * @Date: 2022-03-31 09:46:25
+ * @Author: Yao guan shou
+ * @LastEditors: Yao guan shou
+ * @LastEditTime: 2022-04-02 18:59:41
+ * @FilePath: /sharedb/examples/textarea/client.js
+ * @Description:
+ */
+import sharedb from "sharedb/lib/client";
+// import ottext from "ot-text";
+// import types from "sharedb/lib/types";
 // Open WebSocket connection to ShareDB server
-var ReconnectingWebSocket = require('reconnecting-websocket');
-var socket = new ReconnectingWebSocket('ws://' + window.location.host);
+import ReconnectingWebSocket from "reconnecting-websocket";
+var StringBinding = require("sharedb-string-binding");
+var socket = new ReconnectingWebSocket("ws://" + window.location.host);
 var connection = new sharedb.Connection(socket);
 
-var element = document.querySelector('textarea');
-var statusSpan = document.getElementById('status-span');
-statusSpan.innerHTML = 'Not Connected';
+var element = document.querySelector("textarea");
+var statusSpan = document.getElementById("status-span");
+statusSpan.innerHTML = "Not Connected";
 
-element.style.backgroundColor = 'gray';
-socket.addEventListener('open', function() {
-  statusSpan.innerHTML = 'Connected';
-  element.style.backgroundColor = 'white';
+element.style.backgroundColor = "gray";
+socket.addEventListener("open", function () {
+  statusSpan.innerHTML = "Connected";
+  element.style.backgroundColor = "white";
 });
 
-socket.addEventListener('close', function() {
-  statusSpan.innerHTML = 'Closed';
-  element.style.backgroundColor = 'gray';
+socket.addEventListener("close", function () {
+  statusSpan.innerHTML = "Closed";
+  element.style.backgroundColor = "gray";
 });
 
-socket.addEventListener('error', function() {
-  statusSpan.innerHTML = 'Error';
-  element.style.backgroundColor = 'red';
+socket.addEventListener("error", function () {
+  statusSpan.innerHTML = "Error";
+  element.style.backgroundColor = "red";
 });
 
 // Create local Doc instance mapped to 'examples' collection document with id 'textarea'
-var doc = connection.get('examples', 'textarea');
-doc.subscribe(function(err) {
+var doc = connection.get("examples", "textarea");
+doc.subscribe(function (err) {
   if (err) throw err;
 
-  var binding = new StringBinding(element, doc, ['content']);
+  var binding = new StringBinding(element, doc, ["content"]);
   binding.setup();
 });
--- examples/textarea/webpack.config.js
+++ examples/textarea/webpack.config.js
@@ -2,8 +2,8 @@
  * @Date: 2018-01-25 00:54:00
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-04-02 18:53:55
- * @FilePath: /sharedb/examples/counter/webpack.config.js
+ * @LastEditTime: 2022-04-02 18:50:16
+ * @FilePath: /sharedb/examples/textarea/webpack.config.js
  * @Description:
  */
 var path = require("path");
