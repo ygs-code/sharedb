aa568d52ef8f542ce2b50d60d34a28532fe7e8d2
qq281113270 <2811113270@qq.com>
2022-04-04 01:53:05 +0800

    源码分析

--- examples/textarea/client.js
+++ examples/textarea/client.js
@@ -6,17 +6,24 @@
  * @FilePath: /sharedb/examples/textarea/client.js
  * @Description:
  */
-import sharedb from "sharedb/lib/client";
-// import ottext from "ot-text";
-// import types from "sharedb/lib/types";
-// Open WebSocket connection to ShareDB server
-import ReconnectingWebSocket from "reconnecting-websocket";
+
+// sharedb 协同
+import sharedb from "sharedb/lib/client"; 
+// websocket 连接
+import ReconnectingWebSocket from "reconnecting-websocket"; 
+
+// 绑定
 var StringBinding = require("sharedb-string-binding");
+// socket
 var socket = new ReconnectingWebSocket("ws://" + window.location.host);
+
 var connection = new sharedb.Connection(socket);
 
+// textarea dom
 var element = document.querySelector("textarea");
+//  dom
 var statusSpan = document.getElementById("status-span");
+
 statusSpan.innerHTML = "Not Connected";
 
 element.style.backgroundColor = "gray";
@@ -36,6 +43,7 @@ socket.addEventListener("error", function () {
 });
 
 // Create local Doc instance mapped to 'examples' collection document with id 'textarea'
+//创建本地文档实例映射到id为“textarea”的“examples”集合文档  
 var doc = connection.get("examples", "textarea");
 doc.subscribe(function (err) {
   if (err) throw err;
--- examples/textarea/webpack.config.js
+++ examples/textarea/webpack.config.js
@@ -2,11 +2,22 @@
  * @Date: 2018-01-25 00:54:00
  * @Author: Yao guan shou
  * @LastEditors: Yao guan shou
- * @LastEditTime: 2022-04-02 18:50:16
- * @FilePath: /sharedb/examples/textarea/webpack.config.js
+ * @LastEditTime: 2022-03-31 14:26:47
+ * @FilePath: /sharedb-textarea-example/webpack.config.js
  * @Description:
  */
+require("@babel/polyfill");
+var webpack = require("webpack");
 var path = require("path");
+var WebpackBar = require("webpackbar");
+var FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+var CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+var { CheckerPlugin } = require("awesome-typescript-loader");
+const NODE_ENV = process.env.NODE_ENV; // 环境参数
+//    是否是生产环境
+const isEnvProduction = NODE_ENV === "production";
+//   是否是测试开发环境
+const isEnvDevelopment = NODE_ENV === "development";
 
 module.exports = {
   devtool: "source-map", // 生产环境和开发环境判断
@@ -16,12 +27,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /(\.tsx?$)|(\.ts?$)/,
+        use: ["awesome-typescript-loader"].concat(
+          isEnvDevelopment ? ["thread-loader", "cache-loader"] : []
+        ),
+      },
+      {
         test: /\.m?js$/,
+        enforce: "pre",
+        // 排除文件,因为这些包已经编译过，无需再次编译
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
@@ -31,4 +51,22 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "static"),
   },
+  plugins: [
+    new FriendlyErrorsPlugin(),
+    new CaseSensitivePathsPlugin(),
+    // // 编译ts插件
+    // new CheckerPlugin(),
+    // // 编译进度条
+    // new WebpackBar(),
+    // new webpack.NoEmitOnErrorsPlugin(),
+    // // 如果是配置前端就很好注入插件
+    // new webpack.DefinePlugin({
+    //   //也可以注入插件 能 注入vue 但是不能注入 Koa
+    //   // vue,
+    //   //不能注入 Koa
+    //   // Koa,
+    //   //注入一个环境变量
+    //   "process.env": { BUILD_TARGET: "BUILD_TARGET" },
+    // }),
+  ],
 };
