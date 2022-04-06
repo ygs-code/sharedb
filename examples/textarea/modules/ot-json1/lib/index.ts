98f05a4e8879dbd0e5f0853614e639bb2fbf9671
qq281113270 <2811113270@qq.com>
2022-04-06 23:20:33 +0800

    目录迁移

--- examples/textarea/modules/ot-json1/lib/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './json1.release.js'
-export {ReadCursor, WriteCursor} from './cursor.js'
-export {
-  Doc,
-  JSONOp, JSONOpList, JSONOpComponent,
-  Key, Path, Conflict, ConflictType,
-} from './types'
--- examples/textarea/modules/ot-json1/lib/json1.release.ts
+++
@@ -1,4 +0,0 @@
-// This is a placeholder which will be replaced in the dist directory by the
-// minified version of json1.
-
-export * from './json1'
